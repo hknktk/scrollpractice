dc242f4bd6732c59ae12d333e7e96d5946918fee
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-14 16:22:31 +0900

    commit
    更新した。

---
+++ main.js
@@ -0,0 +1,7 @@
+  {
+    let bg = document.querySelector('#bg');
+    window.addEventListener('scroll',function(){
+      let value = window.scrollY;
+      bg.style.backgroundSize = 100 + value*2 +"px";
+    })
+  }
